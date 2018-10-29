3c36dae75a27f1f7fe8d6d1b37e54ccfbb4f317b
USER1-PC\USER <runitist@gmail.com>
2018-10-29 12:25:18 +0900

    템플릿 완성

---
+++ WebContent/template.jsp
@@ -0,0 +1,34 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%
+	String sectionFlag = request.getAttribute("sectionFlag").toString();
+	String flagTitle = request.getAttribute("flagTitle").toString();
+%>
+<!DOCTYPE>
+<html>
+<head>
+<link rel="stylesheet" href="Common.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title><%=flagTitle%></title>
+</head>
+<body>
+	<div class="container">
+		<header>
+			<h1>타이틀</h1>
+		</header>
+		<div class="mid">
+			<nav>
+				<ul class="navul1">
+					<li><a href="home.do">홈</a></li>
+					<li><a href="">리스트</a></li>
+					<li><a href="">글쓰기</a></li>
+				</ul>
+			</nav>
+			<section>
+				<jsp:include page="<%=sectionFlag%>" />
+			</section>
+		</div>
+		<footer>CopyRight by Lee</footer>
+	</div>
+</body>
+</html>
---
+++ src/kr/co/hk/FrontController.java
@@ -0,0 +1,47 @@
+package kr.co.hk;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FrontController
+ */
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public FrontController() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("[FrontController doGet메서드 실행]");
+		String requri = request.getRequestURI();
+		String ctxpath = request.getContextPath();
+		String curi = requri.substring(ctxpath.length());
+		System.out.println("requri = "+requri+", ctxpath = "+ctxpath+", curi = "+curi);
+		
+		if(curi.equals("/home.do")){
+			request.setAttribute("sectionFlag", "home.jsp");
+			request.setAttribute("flagTitle", "홈");
+			RequestDispatcher rd = request.getRequestDispatcher("template.jsp");
+			rd.forward(request, response);
+		}
+		
+		System.out.println("[FrontController doGet메서드 종료]");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("[FrontController doPost메서드 실행]");
+		request.setCharacterEncoding("UTF-8");
+		doGet(request, response);
+		System.out.println("[FrontController doPost메서드 종료]");
+	}
+
+}
